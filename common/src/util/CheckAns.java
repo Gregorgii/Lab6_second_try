c9c7b7a2bfae5c399289a8b74e39757f66fb53e2
Gregorgii <64372483+Gregorgii@users.noreply.github.com>
2023-09-06 13:14:31 +0300

    parser made and exception mngr

--- common/src/util/CheckAns.java
+++ common/src/util/CheckAns.java
@@ -8,9 +8,9 @@ public class CheckAns {
 
     public static boolean checkAns(String string){
         String bool = string.toLowerCase(Locale.ROOT);
-        if ("yes".equals(bool) || "true".equals(bool)){
+        if ("yes".equals(bool) || "true".equals(bool) || "y".equals(bool)){
             return true;
-        } else if ("no".equals(bool) || "false".equals(bool)){
+        } else if ("no".equals(bool) || "false".equals(bool) || "n".equals(bool)){
             return false;
         } else{
             throw new IllegalArgumentException("Value must be 'yes' or 'no' only");
